963e7b914bc0d7e544ecb2390be65b5ccd1d74c5
MaVi <131026303+302781@users.noreply.github.com>
2024-09-16 14:43:27 -0300

    adicionando codigo

---
+++ logica.java
@@ -0,0 +1,10 @@
+public class logica {
+    public static void main(String[] args) {
+        String nome= "MaVi";
+        int idade = 20;
+        double peso = 72.8;
+
+        System.out.println("o meu nome e" + nome);
+        System.out.println("minha idade e" + idade);
+    }
+}
