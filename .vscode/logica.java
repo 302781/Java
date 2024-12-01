91083c9f618f23d3d94bcd90d24bb1e012f5079e
MaVi <131026303+302781@users.noreply.github.com>
2024-11-30 23:16:33 -0300

    Fazendo uma api de conecçao

--- .vscode/logica.java
+++ .vscode/logica.java
@@ -2,7 +2,7 @@ public class logica {
     public static void main(String[] args) {
         String nome= "MaVi";
         int idade = 20;
-        double peso = 72.8;
+        double peso = 0;
 
         System.out.println("o meu nome e" + nome);
         System.out.println("minha idade e" + idade);
